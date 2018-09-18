9d65c0759bce17845d968b27486242801f9711f3
papunmohanty <papunmohanty31@gmail.com>
2018-09-19 00:01:32 +0530

    Adding styling to the app.py file. inline as well as external url

--- app.py
+++ app.py
@@ -6,26 +6,48 @@ import dash_html_components as html
 
 app = dash.Dash()
 
-app.layout = html.Div(children=[
-    html.H1(children='Hello Dash'),
 
-    html.Div(children='''
-    Dash: A web application framework for Python.
-    '''),
+colors = {
+    'background': '#111111',
+    'text': '#7FDBff'
+}
+
+
+app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
+    html.H1(
+        children='Hello Dash',
+        style={
+            'textAlign': 'center',
+            'color': colors['text']
+        }
+    ),
+
+    html.Div(children='Dash: A web application framework for Python.',
+        style={
+        'textAlign': 'center',
+        'color': colors['text']
+        }
+    ),
 
     dcc.Graph(
-        id='example-graph',
+        id='example-graph-2',
         figure={
             'data': [
                 {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
                 {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montreal'},
             ],
             'layout': {
-                'title': 'Dash Data Visualization'
+                'plot_bgcolor': colors['background'],
+                'paper_bgcolor': colors['background'],
+                'font': {
+                    'color': colors['text']
+                }
             }
         }
     )
 ])
+
+app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 if __name__ == '__main__':
     app.run_server(debug=True)
